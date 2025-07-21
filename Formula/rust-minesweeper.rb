23d179f6c066a70eecc94ad803a6fcc2f1a3bae3
Mohammad-Mohsen <m.m.asemanmanzar@gmail.com>
2025-07-22 03:22:06 +0330

    version v1.1.0

--- Formula/rust-minesweeper.rb
+++ Formula/rust-minesweeper.rb
@@ -1,8 +1,8 @@
 class RustMinesweeper < Formula
   desc "Command-line minesweeper game with mouse support, written in Rust"
   homepage "https://github.com/mohsenasm/rust-minesweeper"
-  url "https://github.com/mohsenasm/rust-minesweeper/releases/download/v1.0.1/minesweeper-macos.tar.gz"
-  sha256 "e65251c0cc9f3f935ace8bed8a8805e6300e68a0f2e73b92b81a726e1e89b584"
+  url "https://github.com/mohsenasm/rust-minesweeper/releases/download/v1.1.0/minesweeper-macos.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "GPL-3.0"
 
   def install
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Mohsenasm Rust-minesweeper
 
-## How do I install these formulae?
+## How do you install these formulae?
 
 `brew install mohsenasm/rust-minesweeper/<formula>`
 
@@ -12,6 +12,14 @@ Or, in a `brew bundle` `Brewfile`:
 tap "mohsenasm/rust-minesweeper"
 brew "<formula>"
 ```
+
+## How do I publish an update?
+
+1. Create `shasum` for the new URL.
+```
+curl -L https://github.com/mohsenasm/rust-minesweeper/releases/download/v1.1.0/minesweeper-macos.tar.gz | shasum -a 256
+```
+2. Update `rust-minesweeper.rb` with the new values.
 
 ## Documentation
 
